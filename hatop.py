50ea2253d6225e6bfbb67d8bfe055fa3c544d02c
John Feuerstein <john@feurix.com>
2010-08-14 19:28:34 +0200

    Add the google code mirror URL

--- hatop.py
+++ hatop.py
@@ -3,6 +3,7 @@
 # Copyright (C) 2010 John Feuerstein <john@feurix.com>
 #
 #   Project URL: http://feurix.org/projects/hatop/
+#    Mirror URL: http://code.google.com/p/hatop/
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
